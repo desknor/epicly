100f6fde539ddd7c175c26a16eaad27c711a1269
desknor <deschdev@gmail.com>
2020-05-14 17:12:57 -0400

    removal of tracks completed

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -19,6 +19,7 @@ class App extends React.Component {
       {name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6},]
     }
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
@@ -31,6 +32,13 @@ class App extends React.Component {
     this.setState({playlistTracks: tracks});
   }
 
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
+  }
+
   render() {
   return (
     <div>
@@ -41,7 +49,8 @@ class App extends React.Component {
         <SearchResults searchResults={this.state.searchResults} 
           onAdd={this.addTrack} />
         <Playlist playlistName={this.state.playlistName} 
-                  playlistTracks={this.state.playlistTracks}/>
+                  playlistTracks={this.state.playlistTracks}
+                  onRemove={this.removeTrack} />
       </div>
      </div>
     </div>
