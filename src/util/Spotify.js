ec307be6a3a409d36c829b802c774dc18f7ddb3e
desknor <deschdev@gmail.com>
2020-05-19 15:33:16 -0400

    search completed

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,7 +1,5 @@
 const clientId = 'f7c3bebd15a9456698c1443ee7384660'
 const redirectUri = 'http://localhost:3000/'
-let accessToken
-
 let accessToken;
 
 const Spotify = {
@@ -12,11 +10,11 @@ const Spotify = {
 
         //check for access token match
         const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-        const expriresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
 
         if (accessTokenMatch && expiresInMatch) {
             accessToken = accessTokenMatch[1];
-            const expiresIn = number(expiresInMatch[1]);
+            const expiresIn = Number(expiresInMatch[1]);
             //This clears the parameters, allowing the user to grab new access token when it expires.
             window.setTimeout(() => accessToken = '', expiresIn * 1000);
             window.history.pushState('Access Token', null, '/');
@@ -25,7 +23,30 @@ const Spotify = {
             const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
             window.location = accessUrl;
         }  
-    }
+    },
+
+    search(term) {
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
+        { headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (!jsonResponse.tracks) {
+                return [];
+            }
+            return jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artist: track.artists[0].name,
+                album: track.album.name,
+                uri: track.uri
+            }));
+        });
+    },
 }
+
 
 export default Spotify;
